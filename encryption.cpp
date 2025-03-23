b633794380dd34d6489c8fd81678637e720d6894
Abhi2839 <143724189+Abhi2839@users.noreply.github.com>
2025-03-23 18:16:45 +0530

    Update

--- encryption.cpp
+++ encryption.cpp
@@ -1,48 +1,92 @@
 #include <iostream>
 #include <fstream>
-#include <openssl/aes.h>
+#include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <cstring>
 
 const unsigned char ENCRYPTION_KEY[32] = "1234567890abcdef1234567890abcdef";
-unsigned char AES_IV[AES_BLOCK_SIZE] = {0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x31, 0x32,
-                                        0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30};
+
+void handleErrors() {
+    std::cerr << "An error occurred during OpenSSL operation." << std::endl;
+    exit(1);
+}
 
 void encryptFile(const std::string &filename) {
-    AES_KEY enc_key;
-    AES_set_encrypt_key(ENCRYPTION_KEY, 256, &enc_key);
+    unsigned char iv[16];
+    if (!RAND_bytes(iv, sizeof(iv))) {
+        handleErrors();
+    }
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        handleErrors();
+    }
+
+    if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, ENCRYPTION_KEY, iv) != 1) {
+        handleErrors();
+    }
 
     std::ifstream infile(filename, std::ios::binary);
     std::ofstream outfile(filename + ".enc", std::ios::binary);
 
-    unsigned char buffer[AES_BLOCK_SIZE];
-    unsigned char cipher[AES_BLOCK_SIZE];
+    outfile.write(reinterpret_cast<const char *>(iv), sizeof(iv));
 
-    while (infile.read(reinterpret_cast<char *>(buffer), AES_BLOCK_SIZE)) {
-        AES_encrypt(buffer, cipher, &enc_key);
-        outfile.write(reinterpret_cast<char *>(cipher), AES_BLOCK_SIZE);
+    unsigned char buffer[1024];
+    unsigned char cipher[1024 + EVP_CIPHER_block_size(EVP_aes_256_cbc())];
+    int len;
+
+    while (infile.read(reinterpret_cast<char *>(buffer), sizeof(buffer))) {
+        if (EVP_EncryptUpdate(ctx, cipher, &len, buffer, infile.gcount()) != 1) {
+            handleErrors();
+        }
+        outfile.write(reinterpret_cast<char *>(cipher), len);
     }
 
+    if (EVP_EncryptFinal_ex(ctx, cipher, &len) != 1) {
+        handleErrors();
+    }
+    outfile.write(reinterpret_cast<char *>(cipher), len);
+
+    EVP_CIPHER_CTX_free(ctx);
     infile.close();
     outfile.close();
     std::cout << "Encryption complete: " << filename << ".enc" << std::endl;
 }
 
 void decryptFile(const std::string &filename) {
-    AES_KEY dec_key;
-    AES_set_decrypt_key(ENCRYPTION_KEY, 256, &dec_key);
+    unsigned char iv[16];
 
     std::ifstream infile(filename, std::ios::binary);
     std::ofstream outfile("decrypted_" + filename, std::ios::binary);
 
-    unsigned char buffer[AES_BLOCK_SIZE];
-    unsigned char plain[AES_BLOCK_SIZE];
+    infile.read(reinterpret_cast<char *>(iv), sizeof(iv));
 
-    while (infile.read(reinterpret_cast<char *>(buffer), AES_BLOCK_SIZE)) {
-        AES_decrypt(buffer, plain, &dec_key);
-        outfile.write(reinterpret_cast<char *>(plain), AES_BLOCK_SIZE);
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        handleErrors();
     }
 
+    if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, ENCRYPTION_KEY, iv) != 1) {
+        handleErrors();
+    }
+
+    unsigned char buffer[1024];
+    unsigned char plain[1024 + EVP_CIPHER_block_size(EVP_aes_256_cbc())];
+    int len;
+
+    while (infile.read(reinterpret_cast<char *>(buffer), sizeof(buffer))) {
+        if (EVP_DecryptUpdate(ctx, plain, &len, buffer, infile.gcount()) != 1) {
+            handleErrors();
+        }
+        outfile.write(reinterpret_cast<char *>(plain), len);
+    }
+
+    if (EVP_DecryptFinal_ex(ctx, plain, &len) != 1) {
+        handleErrors();
+    }
+    outfile.write(reinterpret_cast<char *>(plain), len);
+
+    EVP_CIPHER_CTX_free(ctx);
     infile.close();
     outfile.close();
     std::cout << "Decryption complete: decrypted_" << filename << std::endl;
@@ -55,8 +99,8 @@ int main() {
     encryptFile(filename);
 
     std::cout << "Enter filename to decrypt: ";
-    std::cin >> filename + ".enc";
-    decryptFile(filename + ".enc");
+    std::cin >> filename;
+    decryptFile(filename);
 
     return 0;
 }
