161781e93954cab4442cb8e2c30e52adcacac33e
Abhi2839 <143724189+Abhi2839@users.noreply.github.com>
2025-03-23 18:18:14 +0530

    Update

--- encryption.cpp
+++ encryption.cpp
@@ -8,7 +8,7 @@ const unsigned char ENCRYPTION_KEY[32] = "1234567890abcdef1234567890abcdef";
 
 void handleErrors() {
     std::cerr << "An error occurred during OpenSSL operation." << std::endl;
-    exit(1);
+    exit(EXIT_FAILURE);
 }
 
 void encryptFile(const std::string &filename) {
@@ -17,34 +17,34 @@ void encryptFile(const std::string &filename) {
         handleErrors();
     }
 
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    if (!ctx) {
-        handleErrors();
-    }
-
-    if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, ENCRYPTION_KEY, iv) != 1) {
-        handleErrors();
-    }
-
     std::ifstream infile(filename, std::ios::binary);
     std::ofstream outfile(filename + ".enc", std::ios::binary);
 
+    if (!infile || !outfile) {
+        std::cerr << "Error opening files." << std::endl;
+        return;
+    }
+
     outfile.write(reinterpret_cast<const char *>(iv), sizeof(iv));
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) handleErrors();
+
+    if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, ENCRYPTION_KEY, iv) != 1)
+        handleErrors();
 
     unsigned char buffer[1024];
     unsigned char cipher[1024 + EVP_CIPHER_block_size(EVP_aes_256_cbc())];
     int len;
 
-    while (infile.read(reinterpret_cast<char *>(buffer), sizeof(buffer))) {
-        if (EVP_EncryptUpdate(ctx, cipher, &len, buffer, infile.gcount()) != 1) {
+    while (infile.read(reinterpret_cast<char *>(buffer), sizeof(buffer)) || infile.gcount()) {
+        if (EVP_EncryptUpdate(ctx, cipher, &len, buffer, infile.gcount()) != 1)
             handleErrors();
-        }
         outfile.write(reinterpret_cast<char *>(cipher), len);
     }
 
-    if (EVP_EncryptFinal_ex(ctx, cipher, &len) != 1) {
+    if (EVP_EncryptFinal_ex(ctx, cipher, &len) != 1)
         handleErrors();
-    }
     outfile.write(reinterpret_cast<char *>(cipher), len);
 
     EVP_CIPHER_CTX_free(ctx);
@@ -59,31 +59,31 @@ void decryptFile(const std::string &filename) {
     std::ifstream infile(filename, std::ios::binary);
     std::ofstream outfile("decrypted_" + filename, std::ios::binary);
 
+    if (!infile || !outfile) {
+        std::cerr << "Error opening files." << std::endl;
+        return;
+    }
+
     infile.read(reinterpret_cast<char *>(iv), sizeof(iv));
 
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    if (!ctx) {
-        handleErrors();
-    }
+    if (!ctx) handleErrors();
 
-    if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, ENCRYPTION_KEY, iv) != 1) {
+    if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, ENCRYPTION_KEY, iv) != 1)
         handleErrors();
-    }
 
     unsigned char buffer[1024];
     unsigned char plain[1024 + EVP_CIPHER_block_size(EVP_aes_256_cbc())];
     int len;
 
-    while (infile.read(reinterpret_cast<char *>(buffer), sizeof(buffer))) {
-        if (EVP_DecryptUpdate(ctx, plain, &len, buffer, infile.gcount()) != 1) {
+    while (infile.read(reinterpret_cast<char *>(buffer), sizeof(buffer)) || infile.gcount()) {
+        if (EVP_DecryptUpdate(ctx, plain, &len, buffer, infile.gcount()) != 1)
             handleErrors();
-        }
         outfile.write(reinterpret_cast<char *>(plain), len);
     }
 
-    if (EVP_DecryptFinal_ex(ctx, plain, &len) != 1) {
+    if (EVP_DecryptFinal_ex(ctx, plain, &len) != 1)
         handleErrors();
-    }
     outfile.write(reinterpret_cast<char *>(plain), len);
 
     EVP_CIPHER_CTX_free(ctx);
